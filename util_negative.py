8f737769475c7a7d00e58ba2f1d9da4ac6cc96fe
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-18 16:30:07 +0800

    Update util_negative.py

--- util_negative.py
+++ util_negative.py
@@ -27,3 +27,28 @@ def addNegtiveSamples(directory, fields = ['text', 'label'], shuffle = 1, seed =
 		f.writelines(dataset)
 		
 	return directory
+
+
+import spacy
+import numpy as np
+
+nlp = spacy.load("en_core_web_md")  # make sure to use larger package!
+doc1 = nlp("I like salty fries and hamburgers.")
+doc2 = nlp("Fast food tastes very good.")
+
+# Similarity of two documents
+print(doc1, "<->", doc2, doc1.similarity(doc2))
+
+def spacySimilarityTable(dataset, field = 'text'):
+	sentences = [json.loads(x)[field] for x in dataset]
+	docs = [nlp(sent) for sent in sentences]
+	num = len(dataset)
+	table = np.zeros((num, num), dtype=float)
+	for i in range(num):
+		for j in range(i):
+			similarity = docs[i].similarity(docs[j])
+			table[i, j] = similarity
+			table[j, i] = similarity
+	ind = np.argsort(table, axis=0)
+	
+	return table
