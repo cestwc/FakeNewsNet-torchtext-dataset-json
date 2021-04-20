7007597bd6bf515e027a4a88b62a4357b1a3adbf
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-20 19:21:00 +0800

    Create util_sentence.py

---
+++ util_sentence.py
@@ -0,0 +1,36 @@
+import json
+import random
+from collections import OrderedDict
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def createSentenceCorpus(directory, shuffle = 1, seed = random.randint(1, 1000)):
+	with open(directory, 'r') as f:
+		dataset = f.readlines()
+		
+	sentenceDataset = []
+
+	if shuffle:
+		random.seed(seed)
+		random.shuffle(dataset)
+
+	num = len(dataset)
+
+	for i, datum in enumerate(dataset):
+
+		d = json.loads(datum)
+		
+		doc = nlp(d['text'])
+		
+		for sent in doc.sents:
+			
+			s = {'text':sent.text}
+
+		sentenceDataset.append(json.dumps(positive) + '\n')
+
+	with open(directory, 'w') as f:
+		f.writelines(sentenceDataset)
+
+	return directory
