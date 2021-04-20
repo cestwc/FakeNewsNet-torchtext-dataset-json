f3d38969b69b7045554d9c645e741585d5c1cacd
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-20 21:44:01 +0800

    Update util_sentence.py

--- util_sentence.py
+++ util_sentence.py
@@ -33,5 +33,29 @@ def createSentenceCorpus(directory, shuffle = 1, seed = random.randint(1, 1000))
 
 	with open(directory, 'w') as f:
 		f.writelines(sentenceDataset)
+		
+
+def sentencizeText(directory, shuffle = 1, seed = random.randint(1, 1000)):
+	with open(directory, 'r') as f:
+		dataset = f.readlines()
+		
+	if shuffle:
+		random.seed(seed)
+		random.shuffle(dataset)
+
+	num = len(dataset)
+
+	for i, datum in enumerate(dataset):
+
+		d = json.loads(datum)
+		
+		doc = nlp(d['text'])
+		
+		d['sentences'] = [sent.text for sent in doc.sents]
+
+		dataset[i] = json.dumps(d) + '\n'
+
+	with open(directory, 'w') as f:
+		f.writelines(dataset)
 
 	return directory
