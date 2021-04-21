db3fef572e1ece5bb1f1e4b82853cd313248ae5d
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-21 14:22:37 +0800

    Update util_sentence.py

--- util_sentence.py
+++ util_sentence.py
@@ -7,6 +7,8 @@ spacy.prefer_gpu()
 
 nlp = spacy.load("en_core_web_sm")
 
+import torch
+
 def createSentenceCorpus(directory, shuffle = 1, seed = random.randint(1, 1000)):
 	with open(directory, 'r') as f:
 		dataset = f.readlines()
@@ -59,3 +61,30 @@ def sentencizeText(directory, shuffle = 1, seed = random.randint(1, 1000)):
 		f.writelines(dataset)
 
 	return directory
+
+
+def createSentenceVectorDataset(directory, sent2vec, shuffle = 1, seed = random.randint(1, 1000)):
+	with open(directory, 'r') as f:
+		dataset = f.readlines()
+		
+	if shuffle:
+		random.seed(seed)
+		random.shuffle(dataset)
+
+	num = len(dataset)
+	
+	vectorDataset = []
+
+	for _, datum in enumerate(dataset):
+
+		d = json.loads(datum)
+		
+		v = dict((k, d[k]) for k in ('label'))
+		
+		v['sentences'] = [{sent:sent2vec(sent)} for sent in d['sentences']]
+		
+		vectorDataset.append(v)
+
+	torch.save(vectorDataset, directory.replace('.json', '.pt'))
+
+	return directory.replace('.json', '.pt')
